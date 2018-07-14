527bc829e2a040254af6f61ec8d9e58dc40c5082
Christina Gerard <ChristinaGerard@Christinas-MBP.attlocal.net>
2018-07-14 19:47:26 -0400

    added portfoilio section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,9 @@
 $(document).ready(function () {
     $(".portfolio__greetingText").toggle();
     $(".portfolio__header__animated").toggle();
+    $("#portfolio__about").toggle();
+    $(".divided").toggle();
+    
 
     // Checking if item is being hoverd on
     $(".dot").hover(function () {
@@ -16,13 +19,41 @@ $(document).ready(function () {
         }
     });
 
+
     function fadeIn() {
         $(".portfolio__greetingText").toggle();
         $(".portfolio__header__animated").toggle();
         $(".portfolio__greetingText").addClass("fadeIn");
-        $("..portfolio__header__animated").addClass("fadeIn");
+        $(".portfolio__header__animated").addClass("fadeIn");
     }
 
     setTimeout(fadeIn, 500);
+
+    function slideInUp() {
+        $("#portfolio__about").toggle();
+        $(".divided").toggle();
+        $("#circle").addClass("slideInUp");
+        $(".portfolio__divider").addClass("slideInUp");
+        $("#portfolio__about").addClass("slideInUp");
+    }
+
+    setTimeout(slideInUp, 1000);
+
+    $(".portfolio__title").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+       
+            window.location.hash = hash;
+          });
+        }
+      });
 
 });
